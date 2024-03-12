6c0790a2414fa88df069bec0a0c1e8f3e89aa8e4
LAXMI KUMAR SAHANI <85158969+sahanilxm@users.noreply.github.com>
2024-03-12 11:48:35 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,14 @@ const startServer = () => {
     dbConnect;
     const app = express();
 
-    app.use(cors());
+    app.options('*', (req, res) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        // Respond to the preflight request
+        res.status(200).send();
+    });
     app.use(express.json());
     app.use(express.urlencoded({extended : true}));
     app.use(router);
