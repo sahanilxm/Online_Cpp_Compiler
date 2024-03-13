61d980035598c8fb24cdad9bab192433544ee9e0
sahanilxm <laxmisahani240@gmail.com>
2024-03-13 14:35:16 +0530

    Resolving CORS Issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,13 @@ const startServer = () => {
     dbConnect;
     const app = express();
 
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
+
     app.options('*', cors());
     app.use(express.json());
     app.use(express.urlencoded({extended : true}));
