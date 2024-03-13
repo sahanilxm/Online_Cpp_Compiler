15e2d506fd2b72020dd87479e751281a9671dba9
sahanilxm <laxmisahani240@gmail.com>
2024-03-14 00:59:44 +0530

    Re : Resolving issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,10 +11,21 @@ const startServer = () => {
     const app = express();
 
     app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Credentials", "true");
-		res.setHeader("Access-Control-Allow-Headers", "content-type");
-		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+        res.setHeader(
+          "Access-Control-Allow-Origin",
+          "https://sahanilxm-online-cpp-compiler.vercel.app/"
+        );
+        res.setHeader(
+          "Access-Control-Allow-Methods",
+          "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS, CONNECT, TRACE"
+        );
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+        );
+        res.setHeader("Access-Control-Allow-Credentials", true);
+        res.setHeader("Access-Control-Allow-Private-Network", true);
+        res.setHeader("Access-Control-Max-Age", 7200);
         next();
     });
 
